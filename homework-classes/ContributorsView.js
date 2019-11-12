addcd08f8a437a0cbc2f0b6ac18a3fcebe29b2eb
Mustafa TUNCER <tuncerm@gmail.com>
2019-11-13 00:48:53 +0100

    JS3 Week3 Homework Part2 Completed

--- homework-classes/ContributorsView.js
+++ homework-classes/ContributorsView.js
@@ -19,8 +19,34 @@
      * @param {Object[]} contributors An array of contributor objects
      */
     render(contributors) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('ContributorsView', contributors);
+      this.container.innerHTML = null;
+      const contDetails = createAndAppend('div', this.container, {
+        class: 'main-content',
+      });
+      createAndAppend('p', contDetails, {
+        text: 'Contributions',
+        class: 'c-text',
+      });
+      contributors.forEach(contributor => {
+        const contDiv = createAndAppend('div', contDetails, {
+          class: 'content-item',
+        });
+        const contP = createAndAppend('p', contDiv);
+        createAndAppend('img', contP, {
+          src: contributor.avatar_url,
+          class: 'left avatar',
+        });
+        createAndAppend('a', contP, {
+          text: contributor.login,
+          class: 'text',
+          href: contributor.html_url,
+          target: '_blank',
+        });
+        createAndAppend('span', contP, {
+          text: contributor.contributions,
+          class: 'right medal',
+        });
+      });
     }
   }
 
--- homework-classes/RepoView.js
+++ homework-classes/RepoView.js
@@ -19,8 +19,33 @@
      * @param {Object} repo A repository object.
      */
     render(repo) {
-      // TODO: replace this comment and the console.log with your own code
-      console.log('RepoView', repo);
+      this.container.innerHTML = null;
+      const repoDetails = createAndAppend('div', this.container, {
+        class: 'main-content',
+      });
+      const table = createAndAppend('table', repoDetails);
+      const rRepo = createAndAppend('tr', table);
+      createAndAppend('td', rRepo, { text: 'Repository:', class: 'bold center' });
+      const repoLink = createAndAppend('td', rRepo);
+      createAndAppend('a', repoLink, {
+        href: repo.html_url,
+        target: '_blank',
+        text: repo.name,
+      });
+      const rDesc = createAndAppend('tr', table);
+      createAndAppend('td', rDesc, {
+        text: 'Description:',
+        class: 'bold center',
+      });
+      createAndAppend('td', rDesc, { text: repo.description });
+      const rForks = createAndAppend('tr', table);
+      createAndAppend('td', rForks, { text: 'Forks:', class: 'bold center' });
+      createAndAppend('td', rForks, { text: repo.forks });
+      const rUpdated = createAndAppend('tr', table);
+      createAndAppend('td', rUpdated, { text: 'Updated:', class: 'bold center' });
+      createAndAppend('td', rUpdated, {
+        text: new Date(repo.updated_at).toLocaleString(),
+      });
     }
   }
 
